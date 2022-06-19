7a394a3e3ed1a18c3391e6044eb5f3488bf5e55c
fmakximiano <fernasbae@gmail.com>
2022-06-19 16:31:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR//
+for(int 1= 0,1 <11;++){}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE//
+ int i = 10 while (i >0; --i)
+
+ //3 - Criar um loop que conte todos os números ímpares de 1 até 100
+  for ( int i = 1 && % 1; i < 100; ++){}
+
+  //4- Criar um loop que conte todos os números pares de 0 a 100
+   for (int i = 1&& % 0;1 < 100;++){}
